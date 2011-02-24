50b4e7ada3f82ca759e86a251d4a39794f1d650a
Cedric Levasseur <cedric.levasseur@gmail.com>
2011-02-24 11:32:18 +0100

    loading/parsing conf file in progress

--- src/main/groovy/Bombarde.groovy
+++ src/main/groovy/Bombarde.groovy
@@ -3,25 +3,53 @@ public class Bombarde{
     public static String DEFAULT_CONF_DIR = "src/main/ressources/"
     public static String DEFAULT_CONF_FILE = "mail.in"
 
+    Map<String,String> listOfZipDataFile
 
-    public static void load(){
+
+
+    public Bombarde(){
+        listOfZipDataFile=new HashMap<String,String>()
+    }
+
+    public void load(){
         File conf=new File(DEFAULT_CONF_DIR+DEFAULT_CONF_FILE)
         if(!conf.exists()){
             System.exit(1)
         }
 
-        println conf.text
+        conf.eachLine {
+            line->
+            String result = line.find(~/(\S{2})*\.zip/) //matches("\w\.zip")
+            if(result!=null){
+                String left = result.find(~/[^0-9]*/)
+                if(left.startsWith("http:")){
+                    println left
+                    left=left.find(~/([^\/]*)$/)
+                    println left
+                }
+                String right = result.find(~/[0-9 _.]*.zip/)
+                listOfZipDataFile.put(left,right.replaceFirst("\\.zip\$",""))
+            }
+
+        }
+
+
 
     }
+
+
 
     public static void main(String[] args){
-        println "hello world"
+        println "Bombarde, a BackOffice Mail Archives Decrypter"
         Bombarde bombarde= new Bombarde()
         bombarde.load()
-
+        println bombarde
     }
 
 
 
-
-}
+    public String toString ( ) {
+    return "Bombarde{" +
+    "listOfZipDataFile=" + listOfZipDataFile +
+    '}' ;
+    }}
