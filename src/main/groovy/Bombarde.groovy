d6179c7fbf17bef0b9c79e0fd53c6fbe0d515603
Cedric Levasseur <cedric.levasseur@gmail.com>
2011-02-26 11:33:06 +0100

    method for resetting/initialize a matching file

--- src/main/groovy/Bombarde.groovy
+++ src/main/groovy/Bombarde.groovy
@@ -68,13 +68,6 @@ public class Bombarde {
         bombarde.parse(conf)
         println bombarde
 
-
-        Project p = new Project()
-        p.parse()
-        println p
-
-        bombarde.saveProperties()
-
     }
 
 
@@ -85,14 +78,50 @@ public class Bombarde {
 
     }
 
-    public void saveProperties() {
+    public void resetMatchingFile() {
 
         Properties p = new Properties()
-        p.put("a", "1")
-        p.put("ab", "123")
+
+        p.put("pharma.version", "html-pharma")
+        p.put("para.version", "html-para")
+        p.put("supprimes.version", "SUPPRIMES")
+        p.put("monograph.images.version", "images")
+        p.put("recosDentaires.version", "recoDentaire")
+        p.put("toxin.version", "Toxin")
+        p.put("fit.version", "FIT")
+        p.put("pgr.version", "PGR")
+        p.put("bum.version", "BUM")
+        p.put("rbu.version", "RBU")
+        p.put("useful.info.version", "InfosUtiles")
+        p.put("vidal.vmp.fr.version", "")
+        p.put("iris.version", "FicheIRIS")
+        p.put("vdf.version", "VDF")
+        p.put("vidal.recos.version", "vidalrecos-merlin")
+        p.put("melusine.recos.version", "vidalofficine")
+        p.put("perceval.recos.version", "perceval-recos")
+        p.put("galaad.recos.version", "galaad-recos")
+        p.put("thera.images.version", "CEDRIC")
+        p.put("monos.thera.version", "")
+        p.put("hmk.version", "VDE_HMK")
+        p.put("db.sqlite.vxp.version", "db-vxp")
+        p.put("db.sqlite.vde.version", "db-vde")
+        p.put("db.sqlite.vcd.version", "db-vcd")
+        p.put("db.mysql.vxl.version", "db-mysql-vxl")
+        p.put("db.mysql.vxp.version", "db-mysql-vxp")
+        p.put("db.mysql.vxpper.version", "db-mysql-vxpper")
+        p.put("db.mysql.vxpj.version", "db-mysql-vxpj")
+        p.put("db.mysql.vde.version", "db-mysql-vde")
+        p.put("db.mysql.vdej.version", "db-mysql-vdej")
+        p.put("db.mysql.vcd.version", "db-mysql-vcd")
+        p.put("vcd.index.file.version", "vcd-db-index")
+        p.put("vde.index.file.version", "vde-db-index")
+        p.put("vxp.index.file.version", "vxp-db-index")
+
         def prop = new ConfigSlurper().parse(p)
-        println prop.getClass().getName()
-        new File(DEFAULT_CONF_DIR + "matching.groovy").withWriter {writer -> prop.writeTo(writer)}
+
+        File matching=new File(DEFAULT_CONF_DIR + "matching.groovy")
+        matching.delete()
+        matching.withWriter {writer -> prop.writeTo(writer)}
 
 
     }
--- src/main/ressources/matching.groovy
+++ src/main/ressources/matching.groovy
@@ -1,2 +1,42 @@
-a="1"
-ab="123"
+useful.info.version="InfosUtiles"
+db {
+	sqlite {
+		vde.version="db-vde"
+		vcd.version="db-vcd"
+		vxp.version="db-vxp"
+	}
+	mysql {
+		vxp.version="db-mysql-vxp"
+		vxl.version="db-mysql-vxl"
+		vxpper.version="db-mysql-vxpper"
+		vdej.version="db-mysql-vdej"
+		vde.version="db-mysql-vde"
+		vxpj.version="db-mysql-vxpj"
+		vcd.version="db-mysql-vcd"
+	}
+}
+supprimes.version="SUPPRIMES"
+pharma.version="html-pharma"
+vidal {
+	vmp.fr.version=""
+	recos.version="vidalrecos-merlin"
+}
+galaad.recos.version="galaad-recos"
+vdf.version="VDF"
+iris.version="FicheIRIS"
+vxp.index.file.version="vxp-db-index"
+bum.version="BUM"
+monograph.images.version="images"
+recosDentaires.version="recoDentaire"
+fit.version="FIT"
+vde.index.file.version="vde-db-index"
+thera.images.version="CEDRIC"
+pgr.version="PGR"
+toxin.version="Toxin"
+perceval.recos.version="perceval-recos"
+hmk.version="VDE_HMK"
+melusine.recos.version="vidalofficine"
+vcd.index.file.version="vcd-db-index"
+monos.thera.version=""
+rbu.version="RBU"
+para.version="html-para"
